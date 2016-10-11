674ed9b2a99592003d5e1c08d7f4294c56c3dcfc
grouporg <DSchmid@eduh1832.hs-esslingen.de>
2016-10-11 15:24:28 +0200

    Added Login

---
+++ app/src/main/java/com/example/dschmid/grouporganizer/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.example.dschmid.grouporganizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+
+    }
+}
